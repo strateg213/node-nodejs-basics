fc976ac62c09a3346a48f3c212738718f47203da
strateg213 <strateg213@gmail.com>
2024-01-27 16:32:52 +0300

    feat: upgrade env.js

--- src/cli/env.js
+++ src/cli/env.js
@@ -1,5 +1,18 @@
+import { env } from 'node:process';
+env.RSS_app = 'node';
+env.RSS_school = 'node_js';
+
 const parseEnv = () => {
-    // Write your code here 
+    try {
+        const listEnv = process.env;
+        for (let key in listEnv) {
+            if (key.includes('RSS_')) {
+                console.log(key + '=' + listEnv[key]);
+            }
+        }
+    } catch (error) {
+        throw new Error ('ERROR!');
+    }
 };
 
 parseEnv();
