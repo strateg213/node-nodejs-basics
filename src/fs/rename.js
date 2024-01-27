f6e2386980c094c10518925ab58e5700a1c976fa
strateg213 <strateg213@gmail.com>
2024-01-27 13:00:43 +0300

    fix: remove method in rename.js

--- src/fs/rename.js
+++ src/fs/rename.js
@@ -1,18 +1,18 @@
 import path from 'path';
 import { fileURLToPath } from 'url';
-import { cp } from 'fs/promises';
+import { rename } from 'fs/promises';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 const oldPath = path.join(__dirname, 'files', 'wrongFilename.txt');
 const newPath = path.join(__dirname, 'files', 'properFilename.md');
 
-const rename = async () => {
+const renameFile = async () => {
     try {
-        await cp(oldPath, newPath, { force: false, errorOnExist: true });
+        await rename(oldPath, newPath);
     } catch (error) {
         throw new Error ('FS operation failed');
     }
 };
 
-await rename();
+await renameFile();
