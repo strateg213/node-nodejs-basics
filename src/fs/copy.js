e38fcd1e007bee0fb788ece9f41a1703dfa5aec1
strateg213 <strateg213@gmail.com>
2024-01-27 10:52:44 +0300

    feat: update copy.js

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,5 +1,18 @@
+import path from 'path';
+import { fileURLToPath } from 'url';
+import { cp } from 'fs/promises';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const srcDirectory = path.join(__dirname, 'files');
+const destDirectory = path.join(__dirname, 'files_copy');
+
 const copy = async () => {
-    // Write your code here 
+    try {
+      await cp(srcDirectory, destDirectory, { recursive: true, force: false, errorOnExist: true });
+    } catch (error) {
+        throw new Error ('FS operation failed');
+    }
 };
 
 await copy();
