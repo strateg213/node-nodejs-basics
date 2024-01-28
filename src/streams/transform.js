3ca34d14931fa978e887ab24886cec5b0743fdbc
strateg213 <strateg213@gmail.com>
2024-01-28 16:31:41 +0300

    feat: update transform.js

--- src/streams/transform.js
+++ src/streams/transform.js
@@ -1,5 +1,19 @@
+import { Transform, pipeline } from "node:stream";
+
 const transform = async () => {
-    // Write your code here 
+const input = process.stdin;
+const output = process.stdout;
+const myTransform = new Transform({
+    transform(chunk, encoding, callback) {
+        const cnvInput = chunk.toString().trim();
+        const reversedInput = cnvInput.split('').reverse().join('');
+        callback(null, reversedInput + '\n' );
+    } } );
+    pipeline(input, myTransform, output, (err) => {
+        if (err) {
+            console.log('Error');
+        }
+    });
 };
 
 await transform();
