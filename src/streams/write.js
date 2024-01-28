cef436b13a1535d4145fa736992e8422b2c86813
strateg213 <strateg213@gmail.com>
2024-01-28 14:02:50 +0300

    feat: update write.js

--- src/streams/write.js
+++ src/streams/write.js
@@ -1,5 +1,15 @@
+import path from 'path';
+import { fileURLToPath } from 'url';
+import { createWriteStream } from "node:fs";
+const { stdin } = process;
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const pathFile = path.join(__dirname, 'files', 'fileToWrite.txt');
+
 const write = async () => {
-    // Write your code here 
+  const writeStream = createWriteStream(pathFile);
+  stdin.on('data', (data) => writeStream.write(data));
 };
 
 await write();
