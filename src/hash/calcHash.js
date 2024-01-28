2e22f9d165769f7da9600df9340154c9e84d4090
strateg213 <strateg213@gmail.com>
2024-01-28 13:21:21 +0300

    feat: update calcHash.js

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,17 @@
+import { createReadStream } from 'node:fs';
+import path from "path";
+import { fileURLToPath } from "url";
+const { createHash } = await import('node:crypto');
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const pathFile = path.join(__dirname, 'files', 'fileToCalculateHashFor.txt');
+
+
 const calculateHash = async () => {
-    // Write your code here 
+    const hash = createHash('sha256');
+    const stream = createReadStream(pathFile);
+    stream.on("data", (chunk) => hash.update(chunk));
+    stream.on("end", () => console.log(hash.digest('hex')));
 };
 
 await calculateHash();
