7a8864937b852633fe9b89f239a44f9e5d128c00
strateg213 <strateg213@gmail.com>
2024-01-29 22:43:44 +0300

    feat: update worker.js

--- src/wt/main.js
+++ src/wt/main.js
@@ -1,5 +1,18 @@
+import os from 'node:os';
+import { Worker } from 'node:worker_threads';
+import path from 'path';
+import { fileURLToPath } from 'url';
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const pathFile = path.join(__dirname, 'worker.js');
+
+const worker = new Worker(pathFile, { workerData: os.cpus().length});
+//worker.postMessage(os.cpus().length);
+
 const performCalculations = async () => {
-    // Write your code here
+    worker.on('message', msg => {
+        console.log(msg);
+    });
 };
 
 await performCalculations();
